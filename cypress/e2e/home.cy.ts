c87610544019ada09fb87251634d2f436ca614b1
Daniel Piper <piperdaniel1@gmail.com>
2024-09-12 18:33:24 -0700

    Added new cypress tests and github workflow

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,4 +4,10 @@ describe("The Home Page", () => {
   });
 
   // Add the test for the link to the Hobbies page here
+  it("contains a button to /hobbies", () => {
+    cy.visit("/");
+    cy.get("button").should("exist");
+    cy.get("button").click();
+    cy.url().should("include", "/hobbies");
+  });
 });
